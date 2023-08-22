e5b9f06317b21a3acd9cf74d3bf928c2537ed1dc
NergisRecel <nergisrecel@hotmail.com>
2023-08-22 22:16:57 +0200

    Login test and Base Driver - 1st commit

---
+++ src/Utility/BaseDriver.java
@@ -0,0 +1,25 @@
+package Utility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+
+public class BaseDriver {
+
+    public static WebDriver driver; //singleton driver metod
+
+    static {//public static driver baslat
+         driver =new ChromeDriver();
+         driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+    }
+    public static void BekleveKapat(){
+        MyFunction.wait(5);
+        driver.quit();
+
+    }
+}
---
+++ src/akakce/_04_LoginTest.java
@@ -0,0 +1,37 @@
+package akakce;
+
+import Utility.BaseDriver;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+
+public class _04_LoginTest extends BaseDriver {
+@Test
+    public void LoginTest(){
+
+
+
+    driver.get("https://www.akakce.com/");
+    WebElement login= driver.findElement(By.xpath("//*[text()='Giriş Yap']"));
+    login.click();
+
+    WebElement emailbox=driver.findElement(By.xpath("//input[@id='life']"));
+    emailbox.sendKeys("fdsg@sdf.com");
+
+    WebElement password=driver.findElement(By.xpath("//input[@id='lifp']"));
+    password.sendKeys("123456");
+
+    WebElement rememberme=driver.findElement(By.xpath("//input[@name='lifr']"));
+    rememberme.isSelected();
+
+    WebElement enter= driver.findElement(By.xpath("//input[@id='lfb']"));
+    enter.click();
+
+
+    BekleveKapat();
+    }
+
+
+
+}
